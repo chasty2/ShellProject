35515b8ad0fce451c3aca48e956b9029df251bcc
chasty2 <chasty2@uic.edu>
2019-10-15 20:05:47 -0500

    added chain and interrupts

--- hw3.c
+++ hw3.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <signal.h>
 
 #define BUFFERSIZE 300
 
@@ -34,20 +35,84 @@ char *shellPrompt(char *input)
 //////////////////////////////////////////////////////////////////////////
 
 //
-//// freeCommands: frees memory used for arrays in **commands
+//// sigint_handler: function for catching SIGINT
 //
 
-void freeCommands(char **commands)
+void sigint_handler(int sig)
 {
+  char catchMessage[] = "\ncaught sigint\n";
+  char prompt[] = "CS361 >";
+  write(1,catchMessage,sizeof(catchMessage));
+  write(1,prompt,sizeof(prompt));
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+//
+//// sigstp_handler: function for catching SIGTSTP
+//
+
+void sigtstp_handler(int sig)
+{
+  char catchMessage[] = "\ncaught sigstp\n";
+  char prompt[] = "CS361 >";
+  write(1,catchMessage,sizeof(catchMessage));
+  write(1,prompt,sizeof(prompt));
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+
+//
+//// freeCommands: frees memory used for arrays in **commands
+////               
+//
+
+void freeCommands(char **commands, int size)
+{
+    /*
+    NOTE: if ptr is null, no free occurs. This is a problem because
+    I set commands[size+1][0] = '\0', and it is not being freed.
+    This causes errors downstream. So that is why it is important 
+    to set malloc'ed null pointers to a value before freeing them
+    */
+
+    //commands[*size][0] = 'c';
+
     // loop through arrays & free each
     int i;
-
-    for (i = 0; i < 20; i++)
+    for (i = 0; i < size; i++)
     {
         free(commands[i]);
     }
-    
     return;
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+//
+//// resetFlags: sets size, chainFlag, and pipeFlag to zero
+//
+
+void resetFlags(int *size, int *chainFlag, int *pipeFlag)
+{
+    *size = 0;
+    *chainFlag = 0;
+    *pipeFlag = 0;
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+
+//
+//// freeMemoryAndExit: frees all malloc'd memory and exits shell
+//
+
+void freeMemoryAndExit(char *input, char **commands)
+{
+    free(input);
+    free(commands);
+    exit(0);
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -56,18 +121,15 @@ void freeCommands(char **commands)
 //// checkExitStatus: exit shell if input == "exit"
 //
 
-void checkExitStatus(char *input, char **commands)
+int checkExitStatus(char *input)
 {
     if (strcmp(input, "exit") == 0)
     {
-       free(input);
-       freeCommands(commands);   //free arrays in **commands
-       free(commands);           //free array of arrays (**commands)
-       exit(0);
+        return 0;
     }
     else
     {
-        return;
+        return 1;
     }
 }
 
@@ -79,27 +141,41 @@ void checkExitStatus(char *input, char **commands)
 //// returns. Sets  
 //
 
-char **processInput(char *input, char** commands)
+char **processInput(char *input, char** commands, int *size,
+                                int *chainFlag, int *pipeFlag)
 {
 
     // strtok each command into token and malloc token into the next
     // array position in **commands
     char *token = strtok(input, " ");
-    int i = 0;
+    int i;
 
     while(token)
     {
         commands[i] = (char*)malloc(sizeof(char)*(strlen(token)+1));
         strcpy(commands[i], token);
         commands[i][(strlen(token)+1)] = '\0';
+        
+        // flag ; and | characters
+        if (strchr(token, ';') != NULL)
+        {
+            *chainFlag = i;
+        }
+        else if (strchr(token, '|') != NULL)
+        {
+            *pipeFlag = i;
+        }
 
         token = strtok(NULL, " ");
-        i = i + 1;
+        i++;
     }
     
     //add null character to next array position in **commands
-    commands[(i+1)] = (char*)malloc(sizeof(char));
-    commands[(i+1)][0] = '\0';
+    // NOTE: next position is null character by default
+    //commands[(i)] = (char*)malloc(sizeof(char));
+    //commands[(i)][0] = '\0';
+
+    *size = i;
     return commands;
 }
 
@@ -129,30 +205,103 @@ void processCommand(char **commands)
     return;
 }
 
+//////////////////////////////////////////////////////////////////////////
 
+//
+//// processChain: splits **commands at ; into two arrays, and calls 
+////               processCommand on each
+//
+
+void processChain(char **commands, int chainFlag, int size)
+{
+   //declare array sizes
+   int array1Size = chainFlag + 1;
+   int array2Size = size - chainFlag;
+
+   //malloc each array
+   char **array1 = (char**)malloc(sizeof(char*)*(array1Size));
+   char **array2 = (char**)malloc(sizeof(char*)*(array2Size));
+   
+   //populate array1
+   int i;
+   for(i = 0; i < (array1Size - 1); i++)
+   {
+        array1[i] = (char*)malloc(sizeof(char)*strlen(commands[i]));
+        strcpy(array1[i],commands[i]);
+   }
+    
+   //populate array2 
+   int j = array1Size;
+   for( i = 0; i < (array2Size - 1); i++)
+   {
+        array2[i] = (char*)malloc(sizeof(char)*strlen(commands[j]));
+        strcpy(array2[i],commands[j]);
+        j++;
+   }
+
+   //run commands
+   processCommand(array1);
+   processCommand(array2);
+
+   //free strings
+   freeCommands(array1, (array1Size - 1));
+   freeCommands(array2, (array2Size - 1));
+
+   //free arrays of pointers
+   free(array1);
+   free(array2);
+
+   return;
+
+}
 
 
 //////////////////////////////////////////////////////////////////////////
 
 
+
 int main()
 {
-    // malloc space to hold user input and bash commands
+    //signals
+    signal(SIGINT, sigint_handler);
+    signal(SIGTSTP, sigtstp_handler);
+
+    // malloc space to hold user input and array of command pointers
     char *input = (char*)malloc(sizeof(char)*300);
     char **commands = (char**)malloc(sizeof(char*)*20);
-    //flags
-    //int chainFlag = 0;
-    //int pipeFlag = 0;
+    
+    //size counter for commands array
+    int size = 0;
+
+    // flags
+    int chainFlag = 0;
+    int pipeFlag = 0;
     
     //while loop runs shell until "exit" is input
 	while(1)
-    {  
+    {   
         input = shellPrompt(input);
-        checkExitStatus(input, commands);
         
-        commands = processInput(input, commands);
-        processCommand(commands);
-        
-        freeCommands(commands);
+        if (checkExitStatus(input) == 0)
+        {
+            freeMemoryAndExit(input, commands);
+        }
+        else
+        {
+            commands = processInput(input, commands, &size,
+                                    &chainFlag, &pipeFlag);
+            if (chainFlag != 0)
+            {
+                processChain(commands, chainFlag, size);
+            }
+            else
+            {
+                processCommand(commands);
+            }
+
+        }
+
+        freeCommands(commands, size);
+        resetFlags(&size, &chainFlag, &pipeFlag); 
     }
 }
